1054db81b4ff59b1aa71f5bf0cbd6bcff94090e0
araceli19 <bear@bears-mbp.attlocal.net>
2016-09-06 22:53:40 -0700

    Added .arcconfig and new java class
    Test Plan: arc diff for assingment 1
    
    Reviewers: cuin5880
    
    Subscribers: NathanielDavidson, sorenlassen-csumb
    
    Differential Revision: https://reviews.facebook.net/D63675

---
+++ QuestionsAnswersApp_/src/main/java/edu/csumb/cst438fa16/araceli2/QuestionsAnswersApp_/App.java
@@ -0,0 +1,13 @@
+package edu.csumb.cst438fa16.araceli2.QuestionsAnswersApp_;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
